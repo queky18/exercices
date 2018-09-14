60c600af12f8d4cef9c64bc7902d8cb257dcb987
Adrian Gheorghe <adrian.gh.m.18@gmail.com>
2018-09-14 23:32:11 +0300

    sP220

--- BackgroundGenerator/Webpack_module/app-reduce.js
+++ BackgroundGenerator/Webpack_module/app-reduce.js
@@ -10,3 +10,15 @@ module.exports = function reduce(arr, iteratee) {
     }
     return memo;
 };
+
+// export default function reduce(arr, iteratee) {
+//   let index = 0,
+//   length = arr.length,
+//   memo = arr[index];
+
+//   index += 1;
+//   for(; index < length; index += 1){
+//     memo = iteratee(memo, arr[index]);
+//   }
+//   return memo;
+// }
