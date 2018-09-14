325c9b194d7a75cf879a066461f65df789c98ba6
Adrian Gheorghe <adrian.gh.m.18@gmail.com>
2018-09-14 23:32:54 +0300

    data Added

--- BackgroundGenerator/Modules_ES6/main.js
+++ BackgroundGenerator/Modules_ES6/main.js
@@ -2,6 +2,35 @@
 // main.js
 import sum from "./sum";
 
+const array = [
+    {
+      username: "john",
+      team: "red",
+      score: 5,
+      items: ["ball", "book", "pen"]
+    },
+    {
+      username: "becky",
+      team: "blue",
+      score: 10,
+      items: ["tape", "backpack", "pen"]
+    },
+    {
+      username: "susy",
+      team: "red",
+      score: 55,
+      items: ["ball", "eraser", "pen"]
+    },
+    {
+      username: "tyson",
+      team: "green",
+      score: 1,
+      items: ["book", "pen"]
+    },
+  
+];
+  
+  
 var values = [ 1, 2, 4, 5, 6, 7, 8, 9 ];
 var answer = sum(values);
 
